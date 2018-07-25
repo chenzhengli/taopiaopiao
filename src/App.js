6824f690271e9ff9d5e0553b1998506e2834f8f9
chenzhengli <529715336@qq.com>
2018-07-25 17:44:13 +0800

    2018/07/25-02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route,HashRouter,Switch,Redirect} from "react-router-dom";
+import {Route,HashRouter,Switch,Redirect,BrowserRouter} from "react-router-dom";
 import logo from './logo.svg';
 import Index from './routes/home/Index';
 import './App.css';
@@ -8,13 +8,13 @@ import './test.css';
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <div>
           <Switch>
             <Route path="/" exact component={Index}></Route>
           </Switch>
         </div>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
--- src/routes/home/components/CityLayer.js
+++ src/routes/home/components/CityLayer.js
@@ -11,18 +11,18 @@ class CityLayer extends Component {
                 <div className="cityLayer-content">
                     <div className="cityBlock" id="定位">
                         <div className="cityBlock-label">定位城市</div>
-                        <div className="cityBlockWraper">
+                        <div className="cityBlock-wrapper">
                             <div className="cityBlock-item">杭州</div>
                         </div>
                     </div>
                     <div className="cityBlock" id="热门">
                         <div className="cityBlock-label">热门城市</div>
-                        <div className="cityBlockWraper">
+                        <div className="cityBlock-wrapper">
                           <div className="cityBlock-item">杭州</div>
+                          <div className="cityBlock-item">上海</div>
                           <div className="cityBlock-item">杭州</div>
-                          <div className="cityBlock-item">杭州</div>
-                          <div className="cityBlock-item">杭州</div>
-                          <div className="cityBlock-item">杭州</div>
+                          <div className="cityBlock-item">南京</div>
+                          <div className="cityBlock-item">北京</div>
                         </div>
                     </div>
                   <div className="cityList" id="A">
@@ -32,14 +32,36 @@ class CityLayer extends Component {
                       <li className="cityList-item">阿坝</li>
                       <li className="cityList-item">阿坝</li>
                       <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
+                      <li className="cityList-item">阿坝</li>
                     </ul>
                   </div>
+                    <div className="cityList" id="B">
+                        <div className="cityList-label">B</div>
+                        <ul className="cityList-wrap">
+                            <li className="cityList-item">北京</li>
+                            <li className="cityList-item">北京</li>
+                            <li className="cityList-item">北京</li>
+                            <li className="cityList-item">北京</li>
+                            <li className="cityList-item">北京</li>
+                            <li className="cityList-item">北京</li>
+                            <li className="cityList-item">北京</li>
+                        </ul>
+                    </div>
                 </div>
-              <div className="cityLayer-index">
-                <ul>
-                  <li><a href="#定位">定位</a></li>
-                  <li><a href="#热门">热门</a></li>
-                  <li><a href="#A">A</a></li>
+              <div className="cityLayer-index cityIndex">
+                <ul className="cityIndex-list">
+                  <li className="cityIndex-item"><a href="#定位">定位</a></li>
+                  <li className="cityIndex-item"><a href="#热门">热门</a></li>
+                  <li className="cityIndex-item"><a href="#A">A</a></li>
+                  <li className="cityIndex-item"><a href="#B">B</a></li>
                 </ul>
               </div>
             </div>
